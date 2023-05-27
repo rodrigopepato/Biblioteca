424ceb63e1e7d2e22f4ef49aac0c293d74e5b380
Rodrigo Pepato <132705370+rodrigopepato@users.noreply.github.com>
2023-05-26 23:58:42 -0300

    Update VisaoLivro.java

--- src/apresentacao/VisaoLivro.java
+++ src/apresentacao/VisaoLivro.java
@@ -1,6 +1,15 @@
 package apresentacao;
 
+import java.util.Collection;
+
+import javax.swing.JButton;
+import javax.swing.JComboBox;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+
+import negocio.Autor;
 
 public class VisaoLivro extends JFrame {
 
@@ -8,8 +17,27 @@ public class VisaoLivro extends JFrame {
 	
 	private JPanel objPainel = new JPanel();
 	
+	private JLabel lblTitulo = new JLabel("Titulo");
+	private JTextField txtTitulo = new JTextField();
 	
 	
+	private JLabel lblAutor = new JLabel ("Autor");
+	private JComboBox <String> cboAutor = new JComboBox <String>();
+	private Collection<Autor> colecao = null;
 	
-
-}
+	private JButton btnGravar = new JButton("Gravar");
+	private JButton btnLimpar = new JButton("Limpar");
+	private JButton btnSair = new JButton("Sair");
+	
+	//metodo pricipal de execulção da classe
+	
+	public static void main(String[] args) {
+		new VisaoLivro().setVisible(true);
+	}
+	
+	public VisaoLivro() {
+		setTitle("Cadastro de livros");
+		setSize(450, 250);
+		setDefaultCloseOperation(EXIT_ON_CLOSE);
+		setLocationRelativeTo(null);
+	}
